57fbc69cce13b812e4db445d0b3565a10ad32782
tiaga <10153548+tiaga@users.noreply.github.com>
2025-06-01 19:27:03 +0100

    Update default DNS settings (#14)

--- app/src/main/java/io/nekohasekai/sagernet/database/DataStore.kt
+++ app/src/main/java/io/nekohasekai/sagernet/database/DataStore.kt
@@ -109,7 +109,7 @@ object DataStore : OnPreferenceDataStoreChangeListener {
     var showGroupInNotification by configurationStore.boolean("showGroupInNotification")
 
     var remoteDns by configurationStore.string(Key.REMOTE_DNS) { "tls://dns.google" }
-    var directDns by configurationStore.string(Key.DIRECT_DNS) { "https://223.5.5.5/dns-query" }
+    var directDns by configurationStore.string(Key.DIRECT_DNS) { "local" }
     var enableDnsRouting by configurationStore.boolean(Key.ENABLE_DNS_ROUTING) { false }
     var enableFakeDns by configurationStore.boolean(Key.ENABLE_FAKEDNS)
 
